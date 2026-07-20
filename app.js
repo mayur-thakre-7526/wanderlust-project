615dd51f45881b8e5b258a87f023bc95d8240906
Mayur Thakre <mayurthakre878@gmail.com>
2026-07-20 12:18:50 +0530

    feat: add user authentication

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -53,7 +53,6 @@ app.get("/", (req, res) => {
   res.send("It is working.");
 });
 
-
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -68,11 +67,22 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
-})
+});
+
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "delta-student",
+//   });
+
+//   let registerdUser = await User.register(fakeUser, "helloworld");
+//   res.send(registerdUser);
+// });
 
 // Access listings route and review route
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 // wrong path req handle
 app.all("/*splat", (req, res, next) => {
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const { required } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose").default;
 
 const userSchema = new Schema({
     email: {
@@ -10,6 +10,6 @@ const userSchema = new Schema({
     }
 });
 
-User.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
