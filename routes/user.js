a3d4be9410d958c6d51e1d4012b5d0aba21e7c45
Mayur Thakre <mayurthakre878@gmail.com>
2026-07-21 17:57:14 +0530

    feat: add user logout functionality

--- routes/user.js
+++ routes/user.js
@@ -41,4 +41,16 @@ router.post(
   },
 );
 
+// log out route 
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+
+    req.flash("success", "you are logged out!");
+    res.redirect("/listings");
+  });
+});
+
 module.exports = router;
