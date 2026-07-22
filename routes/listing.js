246e5414ebf1f42a79c90faefcdb94784f9f323a
Mayur Thakre <mayurthakre878@gmail.com>
2026-07-22 16:49:59 +0530

    refactor: implement MVC on all the listings

--- routes/listing.js
+++ routes/listing.js
@@ -7,50 +7,20 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
 // Show All Listings or Index route
-router.get(
-  "/",
-  wrapAsnc(listingController.index),
-);
+router.get("/", wrapAsnc(listingController.index));
 
 // new route or Add new listing
-router.get("/new", isLoggedIn, (req, res) => {
-  res.render("listings/new");
-});
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 // Show perticuler id info or Show Route
-router.get(
-  "/:id",
-  wrapAsnc(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id)
-      .populate({
-        path: "reviews",
-        populate: {
-          path: "author",
-        },
-      })
-      .populate("owner");
-
-    if (!listing) {
-      req.flash("error", "Listing you requested for does not exists!");
-      return res.redirect("/listings");
-    }
-    res.render("listings/show", { listing });
-  }),
-);
+router.get("/:id", wrapAsnc(listingController.showListing));
 
 // Create Route or post request for add a new listing
 router.post(
   "/",
   isLoggedIn,
   validateListing,
-  wrapAsnc(async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    await newListing.save();
-    req.flash("success", "New Listing Created");
-    res.redirect("/listings");
-  }),
+  wrapAsnc(listingController.createListing),
 );
 
 // Edit Route OR Edit form
@@ -58,17 +28,7 @@ router.get(
   "/:id/edit",
   isLoggedIn,
   isOwner,
-  wrapAsnc(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-
-    if (!listing) {
-      req.flash("error", "Listing you requested for does not exists!");
-      return res.redirect("/listings");
-    }
-
-    res.render("listings/edit", { listing });
-  }),
+  wrapAsnc(listingController.renderEditForm),
 );
 
 // Update Route OR SAVE Changes in DB
@@ -77,13 +37,7 @@ router.put(
   isLoggedIn,
   isOwner,
   validateListing,
-  wrapAsnc(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    req.flash("success", "Listing Updated!");
-
-    res.redirect(`/listings/${id}`);
-  }),
+  wrapAsnc(listingController.updateListing),
 );
 
 // Delete Route
@@ -91,12 +45,7 @@ router.delete(
   "/:id",
   isLoggedIn,
   isOwner,
-  wrapAsnc(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndDelete(id);
-    req.flash("success", "Listing Deleted!");
-    res.redirect("/listings");
-  }),
+  wrapAsnc(listingController.destroyListing),
 );
 
 module.exports = router;
