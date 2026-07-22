df6c88f7b038a852b432a97ebf080077a61d1d78
Mayur Thakre <mayurthakre878@gmail.com>
2026-07-22 14:50:24 +0530

    feat: owner added with the review

--- routes/review.js
+++ routes/review.js
@@ -4,15 +4,17 @@ const wrapAsnc = require("../utils/wrapAsnc.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 
 // Review Post Route
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   wrapAsnc(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
 
     await newReview.save();
