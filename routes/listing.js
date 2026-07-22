b5f02675786b9bf95d7d70e54c1664b43652b576
Mayur Thakre <mayurthakre878@gmail.com>
2026-07-22 17:54:19 +0530

    refactor: group signup and login routes with router.route()

--- routes/listing.js
+++ routes/listing.js
@@ -7,21 +7,28 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
 // Show All Listings or Index route
-router.get("/", wrapAsnc(listingController.index));
+// Create Route or post request for add a new listing
+router
+  .route("/")
+  .get(wrapAsnc(listingController.index))
+  .post(isLoggedIn, validateListing, wrapAsnc(listingController.createListing));
 
 // new route or Add new listing
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 // Show perticuler id info or Show Route
-router.get("/:id", wrapAsnc(listingController.showListing));
-
-// Create Route or post request for add a new listing
-router.post(
-  "/",
-  isLoggedIn,
-  validateListing,
-  wrapAsnc(listingController.createListing),
-);
+// Update Route OR SAVE Changes in DB
+// Delete Route
+router
+  .route("/:id")
+  .get(wrapAsnc(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsnc(listingController.updateListing),
+  )
+  .delete(isLoggedIn, isOwner, wrapAsnc(listingController.destroyListing));
 
 // Edit Route OR Edit form
 router.get(
@@ -29,23 +36,6 @@ router.get(
   isLoggedIn,
   isOwner,
   wrapAsnc(listingController.renderEditForm),
-);
-
-// Update Route OR SAVE Changes in DB
-router.put(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsnc(listingController.updateListing),
-);
-
-// Delete Route
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsnc(listingController.destroyListing),
 );
 
 module.exports = router;
