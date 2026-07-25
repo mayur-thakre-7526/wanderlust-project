d868862c8a39c6c77c72868f7bd133d3219d4354
Mayur Thakre <mayurthakre878@gmail.com>
2026-07-25 16:17:53 +0530

    style: improve navbar styling and fix backend request handling

--- app.js
+++ app.js
@@ -55,9 +55,9 @@ const sessionOptions = {
   },
 };
 
-app.get("/", (req, res) => {
-  res.send("It is working.");
-});
+// app.get("/", (req, res) => {
+//   res.send("It is working.");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
