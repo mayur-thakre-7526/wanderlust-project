d7b2ac7271d37dd966a4344d960e80532b008956
Mayur Thakre <mayurthakre878@gmail.com>
2026-07-26 15:07:34 +0530

    feat: done some changes before deployment

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -50,6 +48,9 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
   touchAfter: 24 * 3600,
 });
 
